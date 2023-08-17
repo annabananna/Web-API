1eeebb38c09c07f107e92635b7bec78f436c91dd
annabananna <ana_bogdanovska@yahoo.com>
2023-08-17 23:54:20 +0200

    Add files via upload

---
+++ HW-class02/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_Class02.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAll()
+        {
+            var users = StaticDb.Users;
+
+            return StatusCode(StatusCodes.Status200OK, users);
+        }
+
+        [HttpGet("index")]                //https:localhost:[]/api/users/index
+        public ActionResult<string> GetByIndex(int index)
+        {
+            try
+            {
+                if(index < 0)
+                {
+                    return BadRequest("The value of the index can't be a negative number");
+                }
+
+                if(index >= StaticDb.Users.Count)
+                {
+                    return NotFound($"The user on index {index} doesn't exist, please enter a value between 0 and {StaticDb.Users.Count - 1}");
+                }
+
+                return Ok(StaticDb.Users[index]);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occured, please try again later");
+            }
+        }
+    }
+}
---
+++ HW-class02/StaticDb.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework_Class02
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "User1",
+            "User2",
+            "User3",
+            "User4",
+            "User5"
+        };
+    }
+}
