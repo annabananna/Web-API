ff36fa871ddac2f73aa127d18cb44ec96ad21696
annabananna <ana_bogdanovska@yahoo.com>
2023-08-23 23:28:58 +0200

    Add files via upload

---
+++ HWClass03/HWClass03/Controllers/BooksController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_Class03.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(StaticDb.Books);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error happend, please try again later");
+            }
+        }
+
+        [HttpGet("{index}")]
+        public IActionResult GetByIndex(int index)
+        {
+            try
+            {
+                if(index < 0)
+                {
+                    return BadRequest("Index can't be a negative number");
+                }
+
+                if(index >= StaticDb.Books.Count)
+                {
+                    return NotFound($"The element on index {index} does not exist");
+                }
+
+                return Ok(StaticDb.Books[index]);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error happend, please try again later");
+            }
+        }
+
+        [HttpGet("filter")]
+        public IActionResult FilterBooks(string? author, string? title)
+        {
+            var query = StaticDb.Books;
+
+            if(!string.IsNullOrEmpty(author))
+            {
+                query = query.Where(x => x.Author.ToLower().Contains(author.ToLower())).ToList();
+            }
+
+            if(!string.IsNullOrEmpty(title))
+            {
+                query = query.Where(x => x.Title.ToLower().Contains(title.ToLower())).ToList();
+            }
+
+            return Ok(query);
+        }
+    }
+}
---
+++ HWClass03/HWClass03/StaticDb.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+
+namespace Homework_Class03
+{
+    public static class StaticDb
+    {
+        public static List<Book> Books = new List<Book>
+        {
+            new Book() {Author = "J. K. Rowling", Title = "Harry Potter"},
+            new Book() {Author = "Danielle Steel", Title = "Happiness"},
+            new Book() {Author = "Bonnie Garmus", Title = "Lessons in Chemistry"},
+            new Book() {Author = "Steve McConnell", Title = "Code Complete"},
+            new Book() {Author = "Robert Cecil Martin", Title = "Clean Code"}
+        };
+    }
+}
